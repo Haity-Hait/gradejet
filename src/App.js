d1120270ed821ad6d4e2dafec794486e35c0e47f
Haity Hait <haityhait47@gmail.com>
2023-05-09 16:17:15 +0100

    Done with the database of the landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
+  let isMounted = true
+  fetch("http://localhost:1520/landingpage").then((res) => res.json())
+  .then((data) => console.log(data.courses))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing mode</h1>
     </div>
   );
 }
